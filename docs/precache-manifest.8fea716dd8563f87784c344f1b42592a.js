777cf4b94ba58286b1f0846caecb00ec0166da8b
mj0se <mariajose1907@gmail.com>
2020-05-11 08:29:00 +0200

    Update docs

---
+++ docs/precache-manifest.8fea716dd8563f87784c344f1b42592a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a8fc9431548ba57b5616ef84479ba4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58f1048690049eab373e",
+    "url": "./static/css/main.d0e8cb1c.chunk.css"
+  },
+  {
+    "revision": "e07efe7c7279deda5428",
+    "url": "./static/js/2.19de9674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.19de9674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f1048690049eab373e",
+    "url": "./static/js/main.9711ac95.chunk.js"
+  },
+  {
+    "revision": "de5691ca375b4a6df040",
+    "url": "./static/js/runtime-main.0f5612cb.js"
+  }
+]);
